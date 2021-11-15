693ab957c830eb746c2ac9c7b3f8446e978982ed
student <mpatankar06@gmail.com>
2021-11-15 11:44:45 -0500

    aa

--- src/Factorial.java
+++ src/Factorial.java
@@ -18,15 +18,33 @@ public class Factorial {
             ediff = e - olde;
             i++;
         }
-
         return e;
     }
 
+    private static double calcEX(int x) {
+        double ex = 1;
+        double ediff = 1;
+        int i = 1;
+        while (ediff > 0.001) {
+            double olde = ex;
+            ex += (Math.pow(x, i) / calcFactorial(i));
+            ediff = ex - olde;
+            i++;
+        }
+        return ex;
+    }
+
     public static void main(String[] args) {
+        System.out.println("Factorials 1-20:");
         for (int i = 1; i <= 20; i++) {
             System.out.println(calcFactorial(i));
         }
+        System.out.println("Euler's number");
+        System.out.printf("e is %2.3f \n", calcE());
 
-        System.out.println(calcE());
+        System.out.println("e^x 1-5:");
+        for (int i = 0; i <= 5; i++) {
+            System.out.println(calcEX(i));
+        }
     }
 }
